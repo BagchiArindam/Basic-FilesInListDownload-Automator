80af7cbba8333a66f42190e4149c0679cde7c980
arind <arind@LUMPENSAVARNA>
2023-03-27 14:10:31 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+distinctHrefs.forEach(function(href) {
+    var anchor = document.createElement('a');
+    anchor.setAttribute('href', href);
+    anchor.setAttribute('download', href.substring(href.lastIndexOf('/') + 1).replace(".txt", ".py"));
+    anchor.style.display = 'none';
+    document.body.appendChild(anchor);
+
+    fetch(href)
+      .then(function(response) {
+        if (!response.ok)
+        {
+            throw new Error('HTTP error, status = ' + response.status);
+        }
+        return response.blob();
+    })
+    .then(function(blob) {
+        var url = URL.createObjectURL(blob);
+        anchor.setAttribute('href', url);
+        anchor.click();
+    })
+    .catch (function(error) {
+        console.log('Error: ' + error.message);
+    });
+    });
